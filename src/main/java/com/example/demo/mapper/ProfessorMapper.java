1f372c4335f146578b62089f8fc3e71373a52f25
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-06-07 18:50:13 +0500

    ProfessorMapper updated

--- src/main/java/com/example/demo/mapper/ProfessorMapper.java
+++ src/main/java/com/example/demo/mapper/ProfessorMapper.java
@@ -3,10 +3,18 @@ package com.example.demo.mapper;
 import com.example.demo.dto.ProfessorDto;
 import com.example.demo.entity.Professor;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface ProfessorMapper {
 
+    ProfessorMapper PROFESSOR_MAPPER = Mappers.getMapper(ProfessorMapper.class);
+
     Professor toEntity (ProfessorDto professorDto);
     ProfessorDto toDto (Professor professor);
+
+    List<ProfessorDto> toDto(List<Professor> professor);
+    List<Professor> toEntity(List<ProfessorDto> professorDtoList);
 }
