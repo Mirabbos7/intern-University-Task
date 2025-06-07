2db32dd8106f2e8d93404fd6c47dd6795d7a10e7
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-06-07 18:41:49 +0500

    DisciplineMapper updated

--- src/main/java/com/example/demo/mapper/DisciplineMapper.java
+++ src/main/java/com/example/demo/mapper/DisciplineMapper.java
@@ -3,10 +3,18 @@ package com.example.demo.mapper;
 import com.example.demo.dto.DisciplineDto;
 import com.example.demo.entity.Discipline;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface DisciplineMapper {
 
-    Discipline toDiscipline (DisciplineDto disciplineDto);
+    DisciplineMapper DISCIPLINE_MAPPER = Mappers.getMapper(DisciplineMapper.class);
+
+    Discipline toEntity (DisciplineDto disciplineDto);
     DisciplineDto toDto (Discipline discipline);
+
+    List<DisciplineDto> toDto(List<Discipline> entities);
+    List<Discipline> toEntity(List<DisciplineDto> dtos);
 }
