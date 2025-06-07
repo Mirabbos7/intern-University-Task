954c718254da9b6fad8798e2178a418f3a5ac411
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-06-07 18:51:35 +0500

    StudentMapper updated
    added 2 methods

--- src/main/java/com/example/demo/mapper/StudentMapper.java
+++ src/main/java/com/example/demo/mapper/StudentMapper.java
@@ -3,11 +3,19 @@ package com.example.demo.mapper;
 import com.example.demo.dto.StudentDto;
 import com.example.demo.entity.Student;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface StudentMapper {
 
+    StudentMapper STUDENT_MAPPER = Mappers.getMapper(StudentMapper.class);
+
     Student toEntity(StudentDto studentDto);
 
     StudentDto toDto (Student student);
+
+    List<StudentDto> toDto(List<Student> studentList);
+    List<Student> toEntity(List<StudentDto> studentDtos);
 }
